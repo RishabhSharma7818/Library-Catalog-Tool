c056a1d48442b8db53a5bc1157ad8d541e361287
RishabhSharma7818 <rishabhsharma7818@gmail.com>
2026-06-10 17:12:42 +0530

    refine formatting

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
 public class Main extends LibrarySystem{
     public static void main(String[] args) {
         LibrarySystem library = new LibrarySystem();
+        Scanner sc = new Scanner(System.in);
         while(true){ 
             System.out.println("==============Library Catalog Tool==============");
             System.out.println("1. Add Book");
@@ -26,7 +28,7 @@ public class Main extends LibrarySystem{
             switch(choice){
                 case 1 -> library.addBook();
                 case 2 -> library.deleteBook();
-                case 3 -> library.UpdateBook();
+                case 3 -> library.updateBook();
                 case 4 -> library.issueBook();
                 case 5 -> library.showAvailableBooks();
                 case 6 -> library.showIssuedBooks();
