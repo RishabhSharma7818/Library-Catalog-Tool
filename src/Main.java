731d4f069574fe48930dde270d12aab9c47304e3
RishabhSharma7818 <rishabhsharma7818@gmail.com>
2026-06-12 17:40:47 +0530

    Finalize Main.java file

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,34 @@
-import java.util.Scanner;
-public class Main extends LibrarySystem{
+public class Main {
     public static void main(String[] args) {
         LibrarySystem library = new LibrarySystem();
-        Scanner sc = new Scanner(System.in);
         while(true){ 
             System.out.println("==============Library Catalog Tool==============");
             System.out.println("1. Add Book");
             System.out.println("2. Delete Book");
-            System.out.println("3. Update Book");
-            System.out.println("4. Issue Book");
-            System.out.println("5. Show Available Books");
-            System.out.println("6. Show Issued Books");
-            System.out.println("7. Return Book");
-            System.out.println("8. Show History");
-            System.out.println("9. Exit");
+            System.out.println("3. Issue Book");
+            System.out.println("4. Show Available Books");
+            System.out.println("5. Show Issued Books"); 
+            System.out.println("6. Return Book");
+            System.out.println("7. Show History");
+            System.out.println("8. Exit");
 
             System.out.println("Please enter your choice : ");
-            int choice;
-            try{
-                choice = Integer.parseInt(sc.nextLine());
-            }
-            catch(NumberFormatException e){
-                System.out.println("Invalid Input! Please enter numbers only : ");
-                continue;
-            }
-
+            int choice = library.validIntInput();
+            
             switch(choice){
                 case 1 -> library.addBook();
                 case 2 -> library.deleteBook();
-                case 3 -> library.updateBook();
-                case 4 -> library.issueBook();
-                case 5 -> library.showAvailableBooks();
-                case 6 -> library.showIssuedBooks();
-                case 7 -> library.returnBook();
-                case 8 -> library.showHistory();
-                case 9 -> {
+                case 3 -> library.issueBook();
+                case 4 -> library.showAvailableBooks();
+                case 5 -> library.showIssuedBooks();
+                case 6 -> library.returnBook();
+                case 7 -> library.showHistory();
+                case 8 -> {
                     System.out.println("Exiting...");
-                    sc.close();
                     return;
                 }
                 default -> System.out.println("Invalid Input!");
             }
         }
     }
-}
+}  
