e5857e370f832752d99bfc1d47aef4997f817f5d
RishabhSharma7818 <rishabhsharma7818@gmail.com>
2026-06-12 17:38:45 +0530

    Finalize Book.java file

--- src/Book.java
+++ src/Book.java
@@ -1,27 +1,30 @@
 class Book{
     private final int id;
-    private String title;
-    private String author;
+    private final String title;
+    private final String author;
+    private boolean issued;
 
     public Book(int id, String title, String author){
         this.id = id;
         this.title = title;
         this.author = author;
+        this.issued = false;
     }
 
     @Override
     public String toString(){
         return "ID : " + id +
             "\nTitle : " + title + 
-            "\nAuthor : " + author;
+            "\nAuthor : " + author +
+            "\nStatus : " + (issued ? "Issued" : "Available"); 
     }
 
-    public void setTitle(String title){
-        this.title = title;
+    public void setIssued(boolean issued){
+        this.issued = issued;
     }
 
-    public void setAuthor(String author){
-        this.author = author;
+    public boolean isIssued(){
+        return issued;
     }
 
     public int getId(){
